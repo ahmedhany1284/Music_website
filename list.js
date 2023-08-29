7ce72a386d45db2f9aa7dfc58c3ed1fd46a240c6
ahmedhany1284 <ahmedhany4900@gmail.com>
2023-08-29 22:00:09 +0300

    music web site

---
+++ list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+{
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+},
+{
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+},
+{
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+},
+{
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+},
+{
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+},
+{
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+},
+{
+    name: "Lover",
+    artist: "Tyler Swift ",
+    img: "lover_image",
+    src: "Lover",
+},
+{
+    name: "love story",
+    artist: "Taylor Swift",
+    img: "taylor_swift_love_story",
+    src: "Taylor-Swift-Love-Story",
+},
+{
+    name: "Red",
+    artist: "Tyler Swift ",
+    img: "Taylor_Swift_Red",
+    src: "Taylor-Swift-Red",
+},
+{
+    name: "Wildest-Dreams",
+    artist: "Tyler Swift ",
+    img: "wildest_dreams_taylor",
+    src: "Taylor-Swift-Wildest-Dreams",
+},
+
+];
